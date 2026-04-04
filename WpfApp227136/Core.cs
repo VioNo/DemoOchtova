59bd29772bcd520fa0dbfbc3ed22547c31340100
Muryllebe <katakhovam@gmail.com>
2026-04-04 21:54:39 +0300

    таблица материалы

--- WpfApp227136/Core.cs
+++ WpfApp227136/Core.cs
@@ -8,12 +8,12 @@ namespace WpfApp227136
 {
     internal static class Core
     {
-        public static Entities Context = new Entities();
+        public static  demoEntities Context = new demoEntities();
         public static users AuthUser = null;
     }
     public partial class products
     {
-        public bool IsGreatThen10K => min_price > 10000;
+        public bool IsGreatThen10K => (min_price ?? 0) > 10000m;
         public string Materials => string.Join(", ", product_material.Select(pm => pm.materials.name_material));
 
     }
